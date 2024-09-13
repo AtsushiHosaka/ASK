ae536d89ca9890538152b3ea65f507c2bf26dacc
Atsushi Hosaka <hosaka2917@gmail.com>
2024-09-14 01:13:28 +0900

    Initial Commit

---
+++ ASK/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ASK
+//
+//  Created by 保坂篤志 on 2024/09/14.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
