416a1da8884cb104877f87446000801a44fb7911
Atsushi Hosaka <hosaka2917@gmail.com>
2024-09-14 13:47:40 +0900

    LoginViewを追加

--- ASK/View/ContentView.swift
+++ ASK/View/ContentView.swift
@@ -6,11 +6,33 @@
 //
 
 import SwiftUI
+import SwiftData
+import FirebaseAuth
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @State private var isLoggedIn = false
+    
     var body: some View {
-        QuestionList()
-            .frame(minWidth: 800, minHeight: 600)
+        if isLoggedIn {
+            QuestionList()
+                .frame(minWidth: 800, minHeight: 600)
+        } else {
+            LoginView()
+        }
+    }
+    
+    init() {
+        checkIfLoggedIn()
+    }
+    
+    private func checkIfLoggedIn() {
+        if let currentUser = Auth.auth().currentUser {
+            // すでにログインしているユーザーがいる場合
+            let user = User(email: currentUser.email ?? "", isLoggedIn: true)
+            modelContext.insert(user)
+            isLoggedIn = true
+        }
     }
 }
 
---
+++ ASK/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ASK
+//
+//  Created by 保坂篤志 on 2024/09/14.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
