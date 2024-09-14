41e1327486545d99acdc3c4347dd71d3c5d36b50
Atsushi Hosaka <hosaka2917@gmail.com>
2024-09-14 11:40:45 +0900

    ContentView、SidebarViewを追加

---
+++ ASK/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ASK
+//
+//  Created by 保坂篤志 on 2024/09/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
