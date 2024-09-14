d2366d927d1edf1df2f2686673a77fc1ef903938
Atsushi Hosaka <hosaka2917@gmail.com>
2024-09-14 14:07:19 +0900

    GoogleServiceを更新

--- ASK/View/LoginView.swift
+++ ASK/View/LoginView.swift
@@ -6,10 +6,57 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var errorMessage: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button("Login with Email") {
+                loginWithEmail()
+            }
+            .padding()
+            
+            if let errorMessage = errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            Button("Sign Up with Email") {
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        self.errorMessage = error.localizedDescription
+                    } else {
+                        self.errorMessage = nil
+                        // サインアップ成功
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+    
+    func loginWithEmail() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                self.errorMessage = error.localizedDescription
+            } else {
+                self.errorMessage = nil
+                // ログイン成功
+            }
+        }
     }
 }
 
