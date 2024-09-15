8bb28ed9f58e2ed61eefad10aa56696371d337d9
Atsushi Hosaka <hosaka2917@gmail.com>
2024-09-15 17:46:23 +0900

    MessageRowを追加

---
+++ ASK/View/Message/MessageRow.swift
@@ -0,0 +1,18 @@
+//
+//  MessageRow.swift
+//  ASK
+//
+//  Created by 保坂篤志 on 2024/09/15.
+//
+
+import SwiftUI
+
+struct MessageRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageRow()
+}
